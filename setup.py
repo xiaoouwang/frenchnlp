a5d881e812426551d6fab5b20135f468b812f333
xiaoou <xiaoouwangfrance@gmail.com>
2021-06-07 22:02:24 +0200

    update to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='frenchnlp',
-    version='0.1.9',
+    version='0.2.0',
     description='State of the art toolchain for natural language processing in French',
     long_description_content_type="text/markdown",
     long_description=README,
